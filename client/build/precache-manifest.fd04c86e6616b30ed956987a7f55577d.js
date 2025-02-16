15bfae5523eb5f0de6e037473f2ccc5e92d15d3b
vanhaicddt2 <vanhaicddt2@gmail.com>
2025-02-16 08:00:51 +0700

    update

---
+++ client/build/precache-manifest.fd04c86e6616b30ed956987a7f55577d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4939d5d0b95506b38426cd1cc259df67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7889fbd8e26ed17f77a6",
+    "url": "/static/css/2.fde8feac.chunk.css"
+  },
+  {
+    "revision": "db64ae9eed251e86ec8d",
+    "url": "/static/css/main.dde3a03e.chunk.css"
+  },
+  {
+    "revision": "7889fbd8e26ed17f77a6",
+    "url": "/static/js/2.f7860c37.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/static/js/2.f7860c37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db64ae9eed251e86ec8d",
+    "url": "/static/js/main.31a35e51.chunk.js"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  }
+]);
